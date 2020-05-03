67461e20f16e4a5d37b3eb35a2b49c6fe1b5a54e
potados99 <potados99@gmail.com>
2020-05-04 00:29:14 +0900

    Add test exports.

---
+++ test/exports.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+export NODE_ENV=test
+
+export UAG_PORT_REST=21100
+export UAG_PORT_SLACK=21101
+
+export UIP_PORT=21102
+export UAS_PORT=21103
+
+export WUI_PORT=21104
+export SMS_PORT=21105
+
+export LMS_PORT=21110
+export SNL_PORT=21111
+export AICRO_PORT=21112
+
+export UNIVUC_SSO_SECRET=ssosecret
+export SLACK_SIGNING_SECRET=slacksecret
